614abc21ca56ffe22ed01306cf1dd19894b790e9
YashAgrawalforproject <yashagrawalforproject@gmail.com>
2021-05-26 17:56:51 +0530

    first commit

---
+++ app/src/main/java/com/example/newsapp/ui/gallery/GalleryFragment.java
@@ -0,0 +1,39 @@
+package com.example.newsapp.ui.gallery;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.newsapp.R;
+import com.example.newsapp.WebViewController;
+
+public class GalleryFragment extends Fragment {
+
+    private GalleryViewModel galleryViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        galleryViewModel =
+                new ViewModelProvider(this).get(GalleryViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+        WebView webView = root.findViewById(R.id.webViewCNN);
+        webView.loadUrl("https://www.edition.cnn.com/");
+        webView.setWebViewClient(new WebViewController());
+        //final TextView textView = root.findViewById(R.id.text_gallery);
+        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+          //      textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
